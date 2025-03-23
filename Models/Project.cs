53999a778eb20f275e2b0aa692fc6f580e46dd7d
mohamed kamel <midokamel1111@gmail.com>
2025-03-24 00:32:39 +0200

    Add Project Model

---
+++ Models/Project.cs
@@ -0,0 +1,21 @@
+﻿namespace FreelanceManager.Models
+{
+    public class Project
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public double budget { get; set; }
+
+        public double HourlyRate { get; set; }
+        public string Company {  get; set; }
+        public string Priority { get; set; }
+        public string Description { get; set; }
+
+        public DateTime  StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        
+
+
+    }
+}
