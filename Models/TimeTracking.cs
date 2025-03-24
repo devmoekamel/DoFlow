46dd8bdc1b16202eff6acb32a530d6565aa4fa88
Your Name <you@example.com>
2025-03-24 04:36:43 +0300

    add Time Tracking

---
+++ Models/TimeTracking.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreelanceManager.Models
+{
+    public class TimeTracking
+    {
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } 
+
+        [DataType(DataType.Time)]
+        public TimeSpan Duration { get; set; }
+        [DisplayFormat(DataFormatString = @"{0:hh\:mm}")]
+        public TimeSpan? EstimateTime { get; set; }
+
+        [Required]
+        [ForeignKey("Tasks")] 
+        public int TaskId { get; set; }
+        public virtual Task Tasks { get; set; }
+    }
+}
