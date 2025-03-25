7cf0def1f77655bad3b6a88c3a498717a36ecbe1
Morcos Shehata <Morcos Shehata@DESKTOP-MCAB75S>
2025-03-25 17:53:59 +0200

    Mission Model

---
+++ Models/Mission.cs
@@ -0,0 +1,19 @@
+﻿namespace FreelanceManager.Models
+{
+    public class Mission
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateTime Deadline { get; set; }
+
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
+
+        public Project? Project { get; set; }
+        public List<TimeTracking>? TimeTracking { get; set; }
+
+    }
+}
