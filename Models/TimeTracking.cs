f71b00dc44a019183fb27214e6ac4490c83e2089
Your Name <you@example.com>
2025-03-30 01:37:27 +0300

    add new

--- Models/TimeTracking.cs
+++ Models/TimeTracking.cs
@@ -6,13 +6,11 @@ namespace FreelanceManager.Models
     public class TimeTracking
     {
         public int Id { get; set; }
-       
         public DateTime Date { get; set; } 
         public TimeSpan Duration { get; set; }
-        [DisplayFormat(DataFormatString = @"{0:hh\:mm}")]
         public TimeSpan EstimateTime { get; set; }
         public int TaskId { get; set; }
         [ForeignKey(nameof(TaskId))]
-        public virtual Task Task { get; set; }
+        public virtual Mission Mission { get; set; }
     }
 }
