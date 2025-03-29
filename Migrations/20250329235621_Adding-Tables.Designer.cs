ed4890d273f15cefe8170e2c15e72c73409ebb28
mohamed kamel <midokamel1111@gmail.com>
2025-03-30 01:57:38 +0200

    migration to globaldb

--- Migrations/20250329235621_Adding-Tables.Designer.cs
+++ Migrations/20250329235621_Adding-Tables.Designer.cs
@@ -12,15 +12,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FreelanceManager.Migrations
 {
     [DbContext(typeof(ITIContext))]
-    [Migration("20250326231804_init")]
-    partial class init
+    [Migration("20250329235621_Adding-Tables")]
+    partial class AddingTables
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.14")
+                .HasAnnotation("ProductVersion", "8.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
