9120b7d59fb878aa3e26f40120cb1438a28b377e
mohamed kamel <midokamel1111@gmail.com>
2025-04-01 16:30:25 +0200

    implement iprojectrepo

--- Repositry/ProjectRepo.cs
+++ Repositry/ProjectRepo.cs
@@ -5,13 +5,43 @@ using FreelanceManager.Models;
 
 namespace FreelanceManager.Repositry
 {ّ
-    public class ProjectRepo:GenericRepo<Project>,IProjectRepo
+    public class ProjectRepo:IProjectRepo
     {
         ITIContext context;
 
-        public ProjectRepo(ITIContext context) : base(context)
+        public ProjectRepo(ITIContext context) 
         {
             this.context = context;
         }
-    }
+
+		public void Add(Project obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Project> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Project GetById(int Id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Remove(Project obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Save()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Project obj)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
