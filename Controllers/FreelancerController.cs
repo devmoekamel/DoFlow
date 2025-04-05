fee60a390347aae03c180af1b5321f1f80c22b44
Morcos Shehata <Morcos Shehata@DESKTOP-MCAB75S>
2025-04-06 01:34:37 +0200

    Register&Login

--- Controllers/FreelancerController.cs
+++ Controllers/FreelancerController.cs
@@ -8,7 +8,7 @@ namespace FreelanceManager.Controllers
 		private readonly UserManager<Freelancer> userManager;
 		private readonly SignInManager<Freelancer> signInManager;
 
-		public FreelancerController(UserManager<Freelancer> userManager, SignInManager<Freelancer> signInManager) 
+		public FreelancerController(UserManager<Freelancer> userManager, SignInManager<Freelancer> signInManager)
 		{
 			this.userManager = userManager;
 			this.signInManager = signInManager;
@@ -29,11 +29,11 @@ namespace FreelanceManager.Controllers
 				user.Email = modelFromReq.Email;
 				user.PasswordHash = modelFromReq.Password;
 				user.PhoneNumber = modelFromReq.PhoneNumber;
-				IdentityResult result = await userManager.CreateAsync(user); // saved to database
+				IdentityResult result = await userManager.CreateAsync(user, modelFromReq.Password); // saved to database
 				if (result.Succeeded)
 				{
 					await signInManager.SignInAsync(user, isPersistent: false); // save cookie
-					return RedirectToAction("Index", "Project");
+					return RedirectToAction("Login");
 
 				}
 				else {
@@ -41,10 +41,56 @@ namespace FreelanceManager.Controllers
 					{
 						ModelState.AddModelError("", error.Description);
 					}
-				
+
 				}
 			}
-			return View("Register",modelFromReq);
+			return View("Register", modelFromReq);
 		}
+
+		public async Task<IActionResult> Logout()
+		{
+			await signInManager.SignOutAsync();
+			return RedirectToAction("Login");
+		}
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(FreelancerLoginVM modelFromReq)
+		{
+			if (ModelState.IsValid)
+			{
+				// check
+				Freelancer freelancer = await userManager.FindByNameAsync(modelFromReq.UserName);
+				if (freelancer != null) { 
+					bool found = await userManager.CheckPasswordAsync(freelancer, modelFromReq.Password);
+					if (found)
+					{
+						await signInManager.SignInAsync(freelancer, modelFromReq.RememberMe);
+						return RedirectToAction("Index", "Project");
+
+					}
+					else
+					{
+						ModelState.AddModelError("", "Invalid Account!");
+					}
+				}
+				else
+				{
+					ModelState.AddModelError("", "UserName or Password is Invalid!");
+				}
+
+					return View();
+			}
+			else
+			{
+				return RedirectToAction("Login", modelFromReq);
+			}
+
+		} 
+
 	}
 }
--- ViewModels/Freelancer/FreelancerLoginVM.cs
+++ ViewModels/Freelancer/FreelancerLoginVM.cs
@@ -1,7 +1,14 @@
-﻿namespace FreelanceManager.ViewModels.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreelanceManager.ViewModels.Account
 {
 	public class FreelancerLoginVM
 	{
+		public string UserName { get; set; }
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		
+		public bool RememberMe { get; set; }
 
 	}
 }
