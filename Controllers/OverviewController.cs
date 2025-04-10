d1a737ddc7c4771b0e85d1039611e02d801b0e31
mohamed kamel <midokamel1111@gmail.com>
2025-04-10 19:17:12 +0200

    fix

--- Controllers/OverviewController.cs
+++ Controllers/OverviewController.cs
@@ -12,43 +12,28 @@ namespace FreelanceManager.Controllers
         private readonly IProjectRepo projectRepo;
         private readonly ITimeTrackingRepo timeTrackingRepo;
 
-        public OverviewController(IClientRepo clientRepo,IMissionRepo missionRepo , IProjectRepo projectRepo,ITimeTrackingRepo timeTrackingRepo)
+        public OverviewController(IClientRepo clientRepo, IMissionRepo missionRepo, IProjectRepo projectRepo, ITimeTrackingRepo timeTrackingRepo)
         {
             this.clientRepo = clientRepo;
             this.missionRepo = missionRepo;
             this.projectRepo = projectRepo;
             this.timeTrackingRepo = timeTrackingRepo;
         }
-<<<<<<< HEAD
         public IActionResult Index()
         {
-            string Userid = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value; // this return id of user
+           // string Userid = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value; // this return id of user
 
             OverviewVM overview = new()
-            { ClientsNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).Select(p=>p.Client).Count(),
-              RecentTasks= missionRepo.GetAll().TakeLast(5),
-              RecentProjects =projectRepo.GetAll().TakeLast(5),
-              TasksNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).SelectMany(p=>p.Missions).Count(),
-              ProjectsNum= projectRepo.GetAll().Where(p=>p.FreelancerId==Userid).Count(),
+            {
+            //    ClientsNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).Select(p => p.Client).Count(),
+                RecentTasks = missionRepo.GetAll().TakeLast(5),
+                RecentProjects = projectRepo.GetAll().TakeLast(5),
+               // TasksNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).SelectMany(p => p.Missions).Count(),
+              //  ProjectsNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).Count(),
             };
             return View(overview);
         }
     }
-=======
-		public IActionResult Index()
-		{
-			string Userid = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value; // this return id of user
-
-			OverviewVM overview = new()
-			{
-				ClientsNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).Select(p => p.Client).Count(),
-				RecentTasks = missionRepo.GetAll().TakeLast(5),
-				RecentProjects = projectRepo.GetAll().TakeLast(5),
-				TasksNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).SelectMany(p => p.Missions).Count(),
-				ProjectsNum = projectRepo.GetAll().Where(p => p.FreelancerId == Userid).Count(),
-			};
-			return View(overview);
-		}
-	}
->>>>>>> 652093f27841105ed75fb3098c01d835172f601d
 }
+
+	
